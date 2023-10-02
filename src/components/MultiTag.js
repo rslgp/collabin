c8f0bb7bf8be21d77eba5256e6e843f552b6ada7
rslgp <rslgp@cin.ufpe.br>
2023-10-02 11:42:18 -0300

    fix warning

--- src/components/MultiTag.js
+++ src/components/MultiTag.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import getUsersWithTags from "./func/queryTags";
 import GlobalVariables from "./func/GlobalVariables";
 
